84d129cf0e817eda50da9dbd52183f81fb2f3b71
Roberto Trapletti <roberto.trapletti@student.supsi.ch>
2018-10-26 10:49:16 +0200

    init

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
-import { myModule } from './mymodule';
-import { HelloButton } from './button'
-console.log('main file ok');
+// import { myModule } from './mymodule';
+// import { HelloButton } from './button';
+// console.log('main file ok');
+// console.log('module loaded:', myModule);
 
-console.log('module loaded:', myModule);
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname+'/');
+});
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000!');
+});
