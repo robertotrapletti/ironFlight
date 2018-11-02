4d00fc94bfa9f7ba24ff7bc46369bf8e97a36500
Roberto Trapletti <roberto.trapletti@student.supsi.ch>
2018-11-02 15:56:57 +0100

    hud first design

--- src/main.js
+++ src/main.js
@@ -5,11 +5,32 @@
 
 var express = require('express');
 var app = express();
+let http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname+'/');
 });
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000!');
+http.listen(3001, function () {
+  console.log('Listening on port 3001!');
+});
+
+io.on('connection', (socket) => {
+  console.log('connected');
+  
+  socket.on('disconnect', function(){
+    console.log('disconnected') 
+  });
+ 
+  socket.on('batteryStatus', (batteryStatus) => {
+    console.log(batteryStatus);
+  });
+  
+  socket.on('gyroscope', (gyroscope) => {
+    console.log(gyroscope);
+  });
+  socket.on('db', (db) => {
+    console.log(db);
+  });
 });
